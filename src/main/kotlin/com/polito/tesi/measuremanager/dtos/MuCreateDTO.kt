6b3957a9b2f8e813a88251b4793eda72dbb90021
R3M1d4Cod3r <superchristian9@gmail.com>
2025-06-02 22:11:12 +0200

    fix: MuCreate

--- src/main/kotlin/com/polito/tesi/measuremanager/dtos/MuCreateDTO.kt
+++ src/main/kotlin/com/polito/tesi/measuremanager/dtos/MuCreateDTO.kt
@@ -2,6 +2,6 @@ package com.polito.tesi.measuremanager.dtos
 
 import com.polito.tesi.measuremanager.entities.MeasurementUnit
 
-data class MuCreateDTO(val networkId: Long, val userid:String)
+data class MuCreateDTO(val networkId: Long, val userId:String)
 
 fun MeasurementUnit.toMUCreateDTO()  = MuCreateDTO(networkId,user.userId)

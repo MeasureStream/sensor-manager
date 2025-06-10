de52433ee10f00aa7685b12823e906daad1adbef
R3M1d4Cod3r <superchristian9@gmail.com>
2025-06-10 18:31:15 +0200

    fix: /API/user

--- src/main/kotlin/com/polito/tesi/measuremanager/controllers/UserController.kt
+++ src/main/kotlin/com/polito/tesi/measuremanager/controllers/UserController.kt
@@ -2,12 +2,14 @@ package com.polito.tesi.measuremanager.controllers
 
 import com.polito.tesi.measuremanager.dtos.UserDTO
 import com.polito.tesi.measuremanager.services.UserService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/API/user")
 class UserController(private val us: UserService) {
+    @GetMapping("/","")
     fun list() :List<UserDTO>{
         return us.list()
     }

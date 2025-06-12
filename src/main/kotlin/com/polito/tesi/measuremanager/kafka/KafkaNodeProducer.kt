5c64a0e0b3c760e60af50ff9424bb1e5a83b1ad2
R3M1d4Cod3r <superchristian9@gmail.com>
2025-06-12 17:15:12 +0200

    fix: KafkaNodeProducer

--- src/main/kotlin/com/polito/tesi/measuremanager/kafka/KafkaNodeProducer.kt
+++ src/main/kotlin/com/polito/tesi/measuremanager/kafka/KafkaNodeProducer.kt
@@ -3,7 +3,9 @@ package com.polito.tesi.measuremanager.kafka
 
 import com.polito.tesi.measuremanager.dtos.EventMU
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
 
+@Service
 class KafkaNodeProducer(private val kafkaTemplate: KafkaTemplate<String, Any>) {
 
     fun sendNodeCreate(muEventDTO: EventMU) {

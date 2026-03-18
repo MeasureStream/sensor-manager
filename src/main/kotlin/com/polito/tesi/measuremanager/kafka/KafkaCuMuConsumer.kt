15a5aa3fbc4d1201c69618b53c0155c6ebcc01ac
R3M1d4Cod3r <superchristian9@gmail.com>
2026-03-18 12:33:48 +0100

    FEAT: added debug

--- src/main/kotlin/com/polito/tesi/measuremanager/kafka/KafkaCuMuConsumer.kt
+++ src/main/kotlin/com/polito/tesi/measuremanager/kafka/KafkaCuMuConsumer.kt
@@ -9,10 +9,16 @@ import org.springframework.stereotype.Service
 class KafkaCUMuConsumer(private val measurementUnitService: MeasurementUnitServiceImpl) {
     @KafkaListener(topics = ["mu-registration"])
     fun consumeMuRegistration(dto: MuRegistrationDTO) {
-        measurementUnitService.registerMu(
-            muNetworkId = dto.muId.toLong(),
-            cuNetworkId = dto.cuId.toLong(),
-            muModel = dto.modelMu.toInt()
-        )
+        println("Ricevuto DTO: $dto")
+        try {
+            measurementUnitService.registerMu(
+                muNetworkId = dto.muId.toLong(),
+                cuNetworkId = dto.cuId.toLong(),
+                muModel = dto.modelMu.toInt()
+            )
+        } catch (e: Exception) {
+            println("ERRORE durante l'elaborazione del messaggio: ${e.message}")
+            e.printStackTrace()
+        }
     }
 }

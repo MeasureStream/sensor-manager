48c21f189146d40a6e4c21defaf9bf8abdf6bb65
R3M1d4Cod3r <superchristian9@gmail.com>
2026-03-25 11:54:11 +0100

    FEAT: added NTCTemperatureSensor

--- src/main/kotlin/com/polito/tesi/measuremanager/entities/Sensor.kt
+++ src/main/kotlin/com/polito/tesi/measuremanager/entities/Sensor.kt
@@ -16,55 +16,110 @@ abstract class Sensor(
     var modelName: String,
     @ManyToOne @JoinColumn(name = "mu_id") var measurementUnit: MeasurementUnit?,
     var sensorIndex: Int = 0,
+
+    var PhysVal: Double = -40.0,
+    var ElecVal: Double = 0.0,
+    var SamplingF: Double = 1000.0,
+    var PhyThreshold: Double = -40.0,
+    var isUpperThresholdMax: Boolean = false, //Se è superiore threshold superiore
+    var isLowerThresholdMin: Boolean = false, //Se è inferiore alla threshold minore
+
 )
 
 @Entity
-class NTCSensor : Sensor(modelName = "NTC", measurementUnit = null) {
-    var templateId: Int = 201
+class NTCTemperatureSensor : Sensor(modelName = "NTCTemperatureSensor", measurementUnit = null) {
 
+    // Template params
+    var templateId: Int = 201
     var unitCode: Int = 1 // es. 1 = °C
 
-    var minPhysVal: Double = 0.0
-    var maxPhysVal: Double = 0.0
+    // Sensor params
+    var minPhysVal: Double = -40.0
+    var maxPhysVal: Double = 105.0
+    //var PhysValSI
     var minElecVal: Double = 0.0
     var maxElecVal: Double = 0.0
+    //Var ElecValSI
 
-    var coeffA: Double = 0.0
-    var coeffB: Double = 0.0
-    var coeffC: Double = 0.0
+    var maxSamplingF: Double = 1000.0 //Hz
+    var minSamplingF: Double = 0.000001
+    //var SampligFSI
 
-    var calDate: Long = 0L
-    var calPeriod: Int = 365
+    var minPhyThreshold: Double = -40.0
+    var maxPhyThreshold: Double = 105.0
+    //var PhyThreshold
 
-    @Column(length = 250)
-    var calInitials: String? = null
+    var readingConsumption: Double = 0.5e-9 // C (da verificare meglio: 1mA consumo ADC * T_convrsione = 15 cycle * 32 MHz) = 0.5 nC
+    // var readingConsumptionSI inserire unità di misura
 
+    // Sensor dependent param
     var refResOhm: Double = 100000.0
     var refTempC: Double = 25.0
-    var responseTime: Double = 0.0
-    var selfHeat: Double = 0.0
 
+    var responseTime: Double = 0.16 // s/°C
+    //Var responseTimeSI
+
+    var selfHeat: Double = 0.0
     var exciteNom: Double = 0.0
     var exciteMax: Double = 0.0
 
+    // Metrology params
+    var absUncertainty: Double = 5.0 //LSB
+    //var absUncSI
+    var UncertaintyPDF: String = "uniform"
+    var uB: Double = 2.9 //LSB
+    //var uBSI
+    var K: Double = 2.0  //Coverage factor
+
+    var coeffA: Double = 0.0
+    //var coeffASI
+    var coeffB: Double = 0.0
+    //var coeffBSI
+    var coeffC: Double = 0.0
+    //var coeffCSI
+    var coeffD: Double = 0.0
+    //var coeffDSI
+
     var measLocId: Long = 0L
+    var calDate: Long = 0L
+    var calPeriod: Int = 365
+
+    // ??
+    @Column(length = 250)
+    var calInitials: String? = null
+
+
+
 }
 
 @Entity
 class PressureSensor : Sensor(modelName = "MS5837", measurementUnit = null) {
     var templateId: Int = 202
     var unitCode: Int = 2 // es. 2 = mbar o Pascal
-    // Block A: Calibration Coefficients (C1-C6 dalla PROM del sensore)
-    var coeffC1: Int = 0 // Pressure sensitivity
-    var coeffC2: Int = 0 // Pressure offset
-    var coeffC3: Int = 0 // Temp coeff of pressure sensitivity
-    var coeffC4: Int = 0 // Temp coeff of pressure offset
-    var coeffC5: Int = 0 // Reference temperature
-    var coeffC6: Int = 0 // Temp coeff of temperature
 
-    // Ranges
-    var minPressure: Double = 0.0
-    var maxPressure: Double = 3000.0 // es. 3000 mbar per la versione 30BA
+    // Sensor params
+    var minPhysVal: Double = 0.0
+    var maxPhysVal: Double = 30000.0 // es. 3000 mbar per la versione 30BA
+    //var PhysValSI
+
+    var minElecVal: Double = 0.0
+    var maxElecVal: Double = 0.0
+    //Var ElecValSI
+
+    var maxSamplingF: Double = 50.0 //Hz
+    var minSamplingF: Double = 0.000001 //circa 10 giorni
+    //var SampligFSI
+
+    var minPhyThreshold: Double = 0.0
+    var maxPhyThreshold: Double = 30000.0
+    //var PhyThreshold
+
+    var readingConsumption: Double = 25e-6 // C (1.25mA * 20ms = 25 uC)
+    // var readingConsumptionSI inserire unità di misura
+
+    var standbyConsumption: Double = 0.1 //uA
+    // var standbyConsumptionSI inserire unità di misura
+
 
     // Block B: Metadata
     var calDate: Long = 0L
